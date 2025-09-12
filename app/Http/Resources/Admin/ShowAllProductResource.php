0b2759ed357d05a2e4f4dce450840890dacdf1ed
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-09-12 15:45:58 +0300

    up

--- app/Http/Resources/Admin/ShowAllProductResource.php
+++ app/Http/Resources/Admin/ShowAllProductResource.php
@@ -15,11 +15,11 @@ class ShowAllProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         $totalQuantity = $this->shipments()->sum('quantity');
-        $firstImage = $this->images->first();
+        
         return [
             "id" => $this -> id,
             "barcode" => $this -> barcode,
-            "image" => $firstImage ? $firstImage->image : null, 
+            "mainImage" => $this -> mainImage,
             "name" => $this -> name ,
             'categoryName' => $this->category->name ?? null,
             'brandName' => $this->brand->name ?? null,
