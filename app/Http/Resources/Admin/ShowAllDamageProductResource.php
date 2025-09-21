321354b3e22b5d9bf9cc1afb39267f717c38c1d8
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-09-21 09:35:10 +0300

    up

--- app/Http/Resources/Admin/ShowAllDamageProductResource.php
+++ app/Http/Resources/Admin/ShowAllDamageProductResource.php
@@ -11,12 +11,30 @@ class ShowAllDamageProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id" => $this -> id,
-            'product' => new ProductResource($this->whenLoaded('product')),
-            "quantity" => $this -> quantity,
-            "reason" => $this -> reason,
-            "status" => $this -> status,
-            "creationDate" => $this -> creationDate,
+            "id" => $this->id,
+            "product" => $this->whenLoaded("product", function () {
+                return [
+                    "id" => $this->product->id,
+                    "name" => $this->product->name,
+                    "mainImage" => $this->product->mainImage,
+                    "category" => $this->whenLoaded("product.category", function () {
+                        return [
+                            "id" => $this->product->category->id,
+                            "name" => $this->product->category->name,
+                        ];
+                    }),
+                    "brand" => $this->whenLoaded("product.brand", function () {
+                        return $this->product->brand ? [
+                            "id" => $this->product->brand->id,
+                            "name" => $this->product->brand->name,
+                        ] : null;
+                    }),
+                ];
+            }),
+            "quantity" => $this->quantity,
+            "reason" => $this->reason,
+            "status" => $this->status,
+            "creationDate" => $this->creationDate,
         ];
     }
 }
