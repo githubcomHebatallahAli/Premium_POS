ebcafdd79d54b3b90e843d1413ed16c544cb93a8
Hebatallah Ali <heba.111980.ma@gmail.com>
2025-09-26 19:35:36 +0300

    up

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ require __DIR__ . '/Apis/Auth/auth.php';
 require __DIR__ . '/Apis/Auth/verification.php';
 require __DIR__ . '/Apis/Auth/resetPassword.php';
 require __DIR__ . '/Apis/Admin/role.php';
+require __DIR__ . '/Apis/Admin/permission.php';
+require __DIR__ . '/Apis/Admin/rolePermissions.php';
 require __DIR__ . '/Apis/Admin/admin.php';
 require __DIR__ . '/Apis/Admin/category.php';
 require __DIR__ . '/Apis/Admin/brand.php';
